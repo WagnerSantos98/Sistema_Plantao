d8735eb1ccc0217e9d2c0a8c6eaaf6059576994e
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-07-19 18:26:07 -0300

    feat: finalização dos campos de inserção de dados

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -27,6 +27,14 @@ document.addEventListener('DOMContentLoaded', function(){
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems);
 });
+//Inicialização floating action - tela equipe
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'top',
+    hoverEnabled: false
+  });
+});
 
 
 
