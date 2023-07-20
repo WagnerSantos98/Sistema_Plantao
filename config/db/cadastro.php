99202e1bbeedba924f37973506135fc6fdfe817a
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-07-20 19:41:09 -0300

    refactor(cadastro): inserção de parametro para SQL

--- config/db/cadastro.php
+++ config/db/cadastro.php
@@ -5,7 +5,7 @@ session_start();
 error_reporting(0);
 
 //Cadstro de usuário
-if(isset($_POST['btn-cadastrar'])){
+if(isset($_POST['btn-cadastrar-usuario'])){
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $funcao = $_POST['funcao'];
@@ -14,12 +14,34 @@ if(isset($_POST['btn-cadastrar'])){
     $contato = $_POST['contato'];
     $cpf = $_POST['cpf'];
 
-    $sql_usuario = "INSERT INTO tb_usuarios(nome, enail, funcao, nome_local, identificador, contato, cpf)
+    $sql_usuario = "INSERT INTO tb_usuarios(nome, email, funcao, nome_local, identificador, contato, cpf)
                     VALUES('$nome', '$email', '$funcao', '$nome_local', '$identificador', '$contato', '$cpf');";
                     $sql_usuarios = mysqli_query($con, $sql_usuario);
 }
 
-//
+//Cadastro de função
+if(isset($_POST['btn-cadastrar-funcao'])){
+    $nome_funcao = $_POST['nome_funcao'];
+
+    $sql_funcao = "INSERT INTO tb_funcoes(nome_funcao)
+                   VALUES('$nome_funcao');";
+    $sql_funcoes = mysqli_query($con, $sql_funcao);
+}
+
+//Cadastro de local de trabalho
+if(isset($_POST['btn-cadastrar-local'])){
+    $nome_local = $_POST['nome_local'];
+    $cep = $_POST['cep'];
+    $rua = $_POST['rua'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $uf = $_POST['uf'];
+    $numero = $_POST['numero'];
+
+    $sql_local = "INSERT INTO tb_locais(nome_local, cep, rua, bairro, cidade, uf, numero)
+                    VALUES('$nome_local', '$cep', '$rua', '$cidade', '$uf', '$uf', '$numero');";
+                    $sql_locais = mysqli_query($con, $sql_local);
+}
 
 
 ?>
