9aaa4d6b98b3c6ea543ad30effc9580a424b735e
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-07-21 18:23:58 -0300

    feat(excluir): exclusão de usuário, estruturação

---
+++ config/db/excluir.php
@@ -0,0 +1,21 @@
+<?php
+include_once('../db/conexao.php');
+session_start();
+
+//Exclusão de usuário
+$id_usuario = filter_input(INPUT_GET, 'id_usuario', FILTER_SANITIZE_NUMBER_INT);
+if(!empty($id_usuario)){
+    $sql_excluir_usuario = "DELETE FROM tb_usuarios WHERE id_usuario = '$id_usuario'";
+    $sql_excluir_usuario = mysqli_query($con, $sql_excluir_usuario);
+    if(mysqli_affected_rows($con)){
+        $_SESSION['msg'] = "<p style='color:green;'>Usuário excluído com sucesso.</p>";
+        header("Location: ../content/administrador.php");
+    }else{
+        $_SESSION['msg'] = "<p style='color:red;'>Erro ao excluir usuário.</p>";
+        header("Location: ../content/administrador.php");
+    }
+}else{
+    $_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar um usuário.</p>";
+    header("Location: ../content/administrador.php");
+}
+?>
