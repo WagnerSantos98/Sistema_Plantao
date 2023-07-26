582ac82c9a111696a1c4b4b060c25816e4f96985
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-07-26 17:51:18 -0300

    feat: alteração no fullcalendar

--- assets/js/fullcalendar.js
+++ assets/js/fullcalendar.js
@@ -1,95 +1,52 @@
-//Inicialização fullcalendar
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      locale: 'pt-br',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth'
-      },
-      initialDate: '2023-01-12',
-      navLinks: true, // can click day/week names to navigate views
-      selectable: true,
-      selectMirror: true,
-      select: function(arg) {
-        var title = prompt('Event Title:');
-        if (title) {
-          calendar.addEvent({
-            title: title,
-            start: arg.start,
-            end: arg.end,
-            allDay: arg.allDay
-          })
-        }
-        calendar.unselect()
-      },
-      eventClick: function(arg) {
-        if (confirm('Are you sure you want to delete this event?')) {
-          arg.event.remove()
-        }
-      },
-      editable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2023-01-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2023-01-07',
-          end: '2023-01-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2023-01-09T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2023-01-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2023-01-11',
-          end: '2023-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T10:30:00',
-          end: '2023-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2023-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2023-01-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2023-01-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2023-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2023-01-28'
-        }
-      ]
-    });
+  /* initialize the external events
+  -----------------------------------------------------------------*/
 
-    calendar.render();
+  var containerEl = document.getElementById('external-events-list');
+  new FullCalendar.Draggable(containerEl, {
+    itemSelector: '.fc-event',
+    eventData: function(eventEl) {
+      return {
+        title: eventEl.innerText.trim()
+      }
+    }
+  });
+
+  //// the individual way to do it
+  // var containerEl = document.getElementById('external-events-list');
+  // var eventEls = Array.prototype.slice.call(
+  //   containerEl.querySelectorAll('.fc-event')
+  // );
+  // eventEls.forEach(function(eventEl) {
+  //   new FullCalendar.Draggable(eventEl, {
+  //     eventData: {
+  //       title: eventEl.innerText.trim(),
+  //     }
+  //   });
+  // });
+
+  /* initialize the calendar
+  -----------------------------------------------------------------*/
+
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: 'pt-br',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,listWeek'
+    },
+    editable: true,
+    droppable: true, // this allows things to be dropped onto the calendar
+    drop: function(arg) {
+      // is the "remove after drop" checkbox checked?
+      if (document.getElementById('drop-remove').checked) {
+        // if so, remove the element from the "Draggable Events" list
+        arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+      }
+    }
+  });
+  calendar.render();
+
 });
-
