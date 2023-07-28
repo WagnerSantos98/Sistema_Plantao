97db8857ed9b14de30c07c346960335762aef478
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-07-28 15:24:18 -0300

    feat: criação de rota para updates no banco

---
+++ routes/alteracoes.php
@@ -0,0 +1,9 @@
+<?php
+/*Todos os updates que vão para o banco de dados estão aqui e estão comentados por telas cada update*/
+
+include_once('../db/conexao.php');
+session_start();
+error_reporting(0);
+
+
+?>
